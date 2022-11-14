73110a1f0387d21fae00789c7b34ead39385de30
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 19:48:33 +0900

    1

--- src/day01/Hello/Hello.java
+++ src/day01/Hello/Hello.java
@@ -3,7 +3,7 @@ package day01.Hello;
 public class Hello {
 	//실행 단축키 F11
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		System.out.println("Hello, world");   
 	}
 
 } 
