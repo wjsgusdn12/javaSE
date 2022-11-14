a4dcd2ce2d243e7d5c134b6121bd6e543d7d96a5
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-14 21:49:56 +0900

    자바 기초 2일차

--- src/day02/basic/Mainclass07.java
+++ src/day02/basic/Mainclass07.java
@@ -1,5 +1,20 @@
 package day02.basic;
+/*
+ * [[ 참조형 데이터형(래퍼런스형) ]]
+ * 
+ * 5. String 형 (문자열)
+ *  - 선언하는 방법
+ *  	String  변수명;
+ *  - 문자열을 지정할수 있다.
+ *  
+ *  기본형 - boolean, char, byte, short, int, long, float, double (명령어는 소문자) 
+ * 
+ */
 
 public class Mainclass07 {
-
+	public static void main(String[] args) {
+		// 변수 선언과 동시에 값 대입하기
+		String str = "안녕하세요.";
+		System.out.println("str : " + str);
+	}
 }
