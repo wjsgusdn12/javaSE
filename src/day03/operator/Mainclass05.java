6cf7c9c5e260baf5b4df4697f7fd163a0b348132
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-16 21:47:55 +0900

    w

--- src/day03/operator/Mainclass05.java
+++ src/day03/operator/Mainclass05.java
@@ -5,7 +5,7 @@ package day03.operator;
  *  =, +=, -=, *=, /=, %=
  */
 
-public class MainClass05 {
+public class Mainclass05 {
 	public static void main(String[] args) {
 		// = 대입연산자로 변수 num에 10 저장
 		int num = 10;
--- src/day03/operator/Mainclass06.java
+++ src/day03/operator/Mainclass06.java
@@ -5,7 +5,7 @@ package day03.operator;
  *  조건식 ? 조건식이 참인 경우 : 조건식이 거짓인 경우
  */
 
-public class MainClass06 {
+public class Mainclass06 {
 	public static void main(String[] args) {
 		int num = -10;
 		String result = num > 0 ? "num은 양수" : "num은 음수";
